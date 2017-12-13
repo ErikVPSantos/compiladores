e7ea52878931ee47bcc93c8b9355213fc8d6eb7e
Thainá Paes <paesjusta@gmail.com>
2017-12-13 20:11:51 -0300

    Update and rename includ_OpenTextoInclud_Open.h to includ_OpenTextoInclud_Close.h

--- classes/includ_OpenTextoInclud_Close.h
+++ classes/includ_OpenTextoInclud_Close.h
@@ -9,12 +9,12 @@ class Includ_OpenTextoInclud_Close {
     	Texto *texto_;
 
 	public:
-    	Includ_OpenTextoInclud_Close(Texto texto) : texto_(texto) { }
+    	Includ_OpenTextoInclud_Close(Texto *texto) : texto_(texto) { }
     	virtual ~Includ_OpenTextoInclud_Close() {
     		delete texto_;
     	}
     	Includ_OpenTextoInclud_Close(const Includ_OpenTextoInclud_Close& b) : texto_(b.texto_) { }
-    	Texto texto() const { return texto_; }
+    	Texto *texto() const { return texto_; }
 	
 	void accept(Visitor *v) {
 		v->visit(this);
