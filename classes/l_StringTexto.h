0b3a541765b10a299b7a34402e4062beeaeba484
ErikVPSantos <evps@ecomp.poli.br>
2017-12-13 12:43:50 -0200

    Update l_StringTexto.h

--- classes/l_StringTexto.h
+++ classes/l_StringTexto.h
@@ -15,6 +15,10 @@ class L_StringTexto {
     	}
     	L_StringTexto(const L_StringTexto& b) : texto_(b.texto_) { }
     	Texto texto() const { return texto_; }
+	
+	void accept(Visitor *v) {
+		v->visit(this);
+	};
 
 };
 
