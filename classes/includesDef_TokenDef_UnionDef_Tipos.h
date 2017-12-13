e2211acf3c19a460e7c391150ab18a0b55f0ec26
Thainá Paes <paesjusta@gmail.com>
2017-12-13 20:13:36 -0300

    Update includesDef_TokenDef_UnionDef_Tipos.h
    Adição de ponteiros para evitar erro de compilação.

--- classes/includesDef_TokenDef_UnionDef_Tipos.h
+++ classes/includesDef_TokenDef_UnionDef_Tipos.h
@@ -16,15 +16,15 @@ class IncludesDef_TokenDef_UnionDef_Tipos {
         Def_tipos *defTp_;
 
 	public:
-    	IncludesDef_TokenDef_UnionDef_Tipos(Include inc, Def_token defT, Def_union defU, Def_tipos defTp) : inc_(inc), defT_(defT), defU_(defU), defTp_(defTp) { }
+    	IncludesDef_TokenDef_UnionDef_Tipos(Include *inc, Def_token *defT, Def_union *defU, Def_tipos *defTp) : inc_(inc), defT_(defT), defU_(defU), defTp_(defTp) { }
     	virtual ~IncludesDef_TokenDef_UnionDef_Tipos() {
     		delete inc_, defT_, defU_, defTp_;
     	}
     	IncludesDef_TokenDef_UnionDef_Tipos(const IncludesDef_TokenDef_UnionDef_Tipos& i) : inc_(i.inc_), defT_(i.defT_), defU_(i.defU_), defTp_(i.defTp_) { }
-    	Includes includes() const { return inc_; }
-        Def_token def_token() const { return defT_; }
-        Def_union def_union() const { return defU_; }
-        Def_tipoes def_tipos() const { return defTp_; }
+    	Includes *includes() const { return inc_; }
+        Def_token *def_token() const { return defT_; }
+        Def_union *def_union() const { return defU_; }
+        Def_tipos *def_tipos() const { return defTp_; }
 	
 	void accept(Visitor *v) {
 		v->visit(this);
