55e35f223d006f5407df8355e6aaf9f9d159b865
Thainá Paes <paesjusta@gmail.com>
2017-12-13 20:14:16 -0300

    Update l_StringTexto.h
    Adição de ponteiros para evitar erro de compilação.

--- classes/l_StringTexto.h
+++ classes/l_StringTexto.h
@@ -9,12 +9,12 @@ class L_StringTexto {
     	Texto *texto_;
 
 	public:
-    	L_StringTexto(Texto texto) : texto_(texto) { }
+    	L_StringTexto(Texto *texto) : texto_(texto) { }
     	virtual ~L_StringTexto() {
     		delete texto_;
     	}
     	L_StringTexto(const L_StringTexto& b) : texto_(b.texto_) { }
-    	Texto texto() const { return texto_; }
+    	Texto *texto() const { return texto_; }
 	
 	void accept(Visitor *v) {
 		v->visit(this);
