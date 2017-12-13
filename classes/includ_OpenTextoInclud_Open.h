2cb2913e55a5f92342b03728ce90ee2aeb3c832b
Thainá Paes <paesjusta@gmail.com>
2017-12-13 17:52:41 -0300

    Update includ_OpenTextoInclud_Open.h
    O nome estava diferente no sintático.

--- classes/includ_OpenTextoInclud_Open.h
+++ classes/includ_OpenTextoInclud_Open.h
@@ -1,19 +1,19 @@
-#ifndef INCLUDOPENTEXTOINCLUDOPEN_H
-#define INCLUDOPENTEXTOINCLUDOPEN_H
+#ifndef INCLUDOPENTEXTOINCLUDCLOSE_H
+#define INCLUDOPENTEXTOINCLUDCLOSE_H
 
 #include "texto.h"
 
-class Includ_OpenTextoInclud_Open {
+class Includ_OpenTextoInclud_Close {
 	
 	private:
     	Texto *texto_;
 
 	public:
-    	Includ_OpenTextoInclud_Open(Texto texto) : texto_(texto) { }
-    	virtual ~Includ_OpenTextoInclud_Open() {
+    	Includ_OpenTextoInclud_Close(Texto texto) : texto_(texto) { }
+    	virtual ~Includ_OpenTextoInclud_Close() {
     		delete texto_;
     	}
-    	Includ_OpenTextoInclud_Open(const Includ_OpenTextoInclud_Open& b) : texto_(b.texto_) { }
+    	Includ_OpenTextoInclud_Close(const Includ_OpenTextoInclud_Close& b) : texto_(b.texto_) { }
     	Texto texto() const { return texto_; }
 	
 	void accept(Visitor *v) {
