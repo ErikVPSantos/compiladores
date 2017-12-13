5483f6d778731b1f77b13e0f0b016a2ee05b5317
Thainá Paes <paesjusta@gmail.com>
2017-12-13 20:03:36 -0300

    Update idColonLinhasSemicolon.h
    Adição de ponteiros para evitar erro de compilação.

--- classes/idColonLinhasSemicolon.h
+++ classes/idColonLinhasSemicolon.h
@@ -2,20 +2,21 @@
 #define IDCOLONLINHASSEMICOLON_H;
 
 #include "linhas.h"
+#include <string>
 
 class IdColonLinhasSemicolon {
 
 	private:
-		ID id_;
+		std::string id_;
 		Linhas *linhas_;
 
  	public:
-    	IdDouble_DotsLinhasDot_Comma(ID id, Linhas linhas) : id_(id), linhas_(linhas) {}
+    	IdDouble_DotsLinhasDot_Comma(std::string id, Linhas *linhas) : id_(id), linhas_(linhas) {}
     	virtual ~IdDouble_DotsLinhasDot_Comma(){
     		delete linhas_;
     	}
     	IdDouble_DotsLinhasDot_Comma(const IdDouble_DotsLinhasDot_Comma& i) : id_(i.id), linhas_(i.linhas_) {}
-    	Linhas linhas() const { return linhas_; }
+    	Linhas *linhas() const { return linhas_; }
 	
 	void accept(Visitor *v) {
 		v->visit(this);
