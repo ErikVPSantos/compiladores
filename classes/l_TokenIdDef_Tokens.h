ffce3d72aca96e7d4707e7a00a735644246295ef
Thainá Paes <paesjusta@gmail.com>
2017-12-13 20:16:47 -0300

    Update l_TokenIdDef_Tokens.h
    Adição de ponteiros para evitar erro de compilação.

--- classes/l_TokenIdDef_Tokens.h
+++ classes/l_TokenIdDef_Tokens.h
@@ -3,21 +3,21 @@
 
 #include "id.h"
 #include "def_tokens.h"
+#include <string>
 
 class L_TokenIdDef_Tokens {
 	
 	private:
     	Def_tokens *defT_;
-        Id *id_;
+	std::string id_;
 
 	public:
-    	L_TokenIdDef_Tokens(Def_tokens defT, Id id) : defT_(defT), id_(id) { }
+    	L_TokenIdDef_Tokens(Def_tokens *defT, std::string id) : defT_(defT), id_(id) { }
     	virtual ~L_TokenIdDef_Tokens() {
     		delete defT_, id_;
     	}
     	L_TokenIdDef_Tokens(const L_TokenIdDef_Tokens& b) : defT_(b.defT_), id_(b.id_) { }
-    	Def_tokens def_tokens() const { return defT_; }
-        Id id() const { return id_; }
+    	Def_tokens *def_tokens() const { return defT_; }
 	
 	void accept(Visitor *v) {
 		v->visit(this);
