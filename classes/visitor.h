456603ae7f96afdcd6fa42cd773191639d7f2cfb
Paula Pithon <paulapithon@gmail.com>
2017-12-20 09:42:22 -0300

    Modificações na classe visitor

--- classes/visitor.h
+++ classes/visitor.h
@@ -34,32 +34,32 @@
 class Visitor {
 
   public:
-    virtual ~visit(Bloco *bloco) {}
-    virtual ~visit(BlocoExps *blocoExps) {}
-    virtual ~visit(Def_Tipos *def_Tipos) {}
-    virtual ~visit(Def_Tokens *def_Tokens) {}
-    virtual ~visit(Def_Union *def_Union) {}
-    virtual ~visit(Exps  *exps) {}
-    virtual ~visit(IdColonLinhasSemicolon  *idColonLinhasSemicolon ) {}
-    virtual ~visit(IdPalavras  *idPalavras ) {}
-    virtual ~visit(IdFolha  *idFolha ) {}
-    virtual ~visit(Includ_OpenTextoInclud_Open   *includ_OpenTextoInclud_Open ) {}
-    virtual ~visit(Includes  *includes ) {}
-    virtual ~visit(IncludesDef_TokenDef_UnionDef_Tipos *includesDef_TokenDef_UnionDef_Tipos) {}
-    virtual ~visit(L_StringTexto *l_StringTexto  ) {}
-    virtual ~visit(L_TokenIdDef_Tokens *l_TokenIdDef_Tokens  ) {}
-    virtual ~visit(L_TokensID *l_TokensID) {}
-    virtual ~visit(L_TypeOp_LTTextoOp_GtId   *l_TypeOp_LTTextoOp_GtId  ) {}
-    virtual ~visit(L_TypeOp_LTTextoOp_GtIdDef_Tipos  *l_TypeOp_LTTextoOp_GtIdDef_Tipos) {}
-    virtual ~visit(L_UnionC_Bracket_LeftTextoC_Bracket_Right  *l_UnionC_Bracket_LeftTextoC_Bracket_Right  ) {}
-    virtual ~visit(Linha    *linha   ) {}
-    virtual ~visit(LinhaLinhas   *linhaLinhas  ) {}
-    virtual ~visit(LinhaUnica   *linhaUnica  ) {}
-    virtual ~visit(Linhas   *linhas   ) {}
-    virtual ~visit(Palavras    *palavras   ) {}
-    virtual ~visit(Preambulo    *preambulo  ) {}
-    virtual ~visit(PreambuloSeparatorExpsSeparator    *preambuloSeparatorExpsSeparator   ) {}
-    virtual ~visit(Texto    *texto   ) {}
+    virtual visit(Bloco *bloco) = 0;
+    virtual visit(BlocoExps *blocoExps) = 0;
+    virtual visit(Def_Tipos *def_Tipos) = 0;
+    virtual visit(Def_Tokens *def_Tokens) = 0;
+    virtual visit(Def_Union *def_Union) = 0;
+    virtual visit(Exps  *exps) = 0;
+    virtual visit(IdColonLinhasSemicolon  *idColonLinhasSemicolon ) = 0;
+    virtual visit(IdPalavras  *idPalavras ) = 0;
+    virtual visit(IdFolha  *idFolha ) = 0;
+    virtual visit(Includ_OpenTextoInclud_Open   *includ_OpenTextoInclud_Open ) = 0;
+    virtual visit(Includes  *includes ) = 0;
+    virtual visit(IncludesDef_TokenDef_UnionDef_Tipos *includesDef_TokenDef_UnionDef_Tipos) = 0;
+    virtual visit(L_StringTexto *l_StringTexto  ) = 0;
+    virtual visit(L_TokenIdDef_Tokens *l_TokenIdDef_Tokens  ) = 0;
+    virtual visit(L_TokensID *l_TokensID) = 0;
+    virtual visit(L_TypeOp_LTTextoOp_GtId   *l_TypeOp_LTTextoOp_GtId  ) = 0;
+    virtual visit(L_TypeOp_LTTextoOp_GtIdDef_Tipos  *l_TypeOp_LTTextoOp_GtIdDef_Tipos) = 0;
+    virtual visit(L_UnionC_Bracket_LeftTextoC_Bracket_Right  *l_UnionC_Bracket_LeftTextoC_Bracket_Right  ) = 0;
+    virtual visit(Linha    *linha   ) = 0;
+    virtual visit(LinhaLinhas   *linhaLinhas  ) = 0;
+    virtual visit(LinhaUnica   *linhaUnica  ) = 0;
+    virtual visit(Linhas   *linhas   ) = 0;
+    virtual visit(Palavras    *palavras   ) = 0;
+    virtual visit(Preambulo    *preambulo  ) = 0;
+    virtual visit(PreambuloSeparatorExpsSeparator    *preambuloSeparatorExpsSeparator   ) = 0;
+    virtual visit(Texto    *texto   ) = 0;
     
 }; 
 
